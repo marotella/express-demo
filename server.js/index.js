c85fb6d45d12d7b97340cb207756a182915b1ad3
marotella <michelle.a.rotella@gmail.com>
2024-01-16 20:48:48 -0500

    get route completed for users

--- server.js/index.js
+++ server.js/index.js
@@ -7,4 +7,11 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.get("/api/users", (req, res) =>{
+    let friends = ['Nitin', 'Eric', 'Jerry', 'Cameron', 'Riley'];
+    res.status(200).send(friends);
+})
+
 app.listen(4000, () => console.log("Server is running on 4000."));
+
+
